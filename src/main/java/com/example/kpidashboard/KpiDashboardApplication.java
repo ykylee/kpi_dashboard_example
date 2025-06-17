cc9b140b05c0dfef84606ce533d6f99f50a92033
ykylee <144748346+ykylee@users.noreply.github.com>
2025-06-17 16:12:06 +0900

    Switch build to Maven

---
+++ src/main/java/com/example/kpidashboard/KpiDashboardApplication.java
@@ -0,0 +1,11 @@
+package com.example.kpidashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KpiDashboardApplication.class, args);
+    }
+}
